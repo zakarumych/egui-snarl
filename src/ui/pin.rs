79fa2195430570fc10a847eae0b74a277ae60721
nrot <nrot13@gmail.com>
2024-01-07 23:48:16 +0300

    Add star to shape

--- src/ui/pin.rs
+++ src/ui/pin.rs
@@ -19,6 +19,9 @@ pub enum PinShape {
     /// Square shape.
     Square,
 
+    /// Star
+    Star,
+
     /// Custom shape.
     Custom(Box<dyn FnOnce(&Painter, Rect, Color32, Stroke)>),
 }
@@ -120,6 +123,30 @@ pub fn draw_pin(painter: &Painter, pin: PinInfo, pos: Pos2, base_size: f32) {
                 stroke: pin.stroke,
             }));
         }
+
+        PinShape::Star => {
+            let points = vec![
+                pos + (vec2(12.0, 2.0) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(15.09, 8.26) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(22.0, 9.27) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(17.0, 14.14) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(18.18, 21.02) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(12.0, 17.77) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(5.82, 21.02) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(7.0, 14.14) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(2.0, 9.27) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(8.91, 8.26) - vec2(12.0, 12.0)) / 22.0 * size,
+                pos + (vec2(12.0, 2.0) - vec2(12.0, 12.0)) / 22.0 * size,
+            ];
+
+            painter.add(Shape::Path(PathShape {
+                points,
+                closed: true,
+                fill: pin.fill,
+                stroke: pin.stroke,
+            }));
+        }
+
         PinShape::Custom(f) => f(
             painter,
             Rect::from_center_size(pos, vec2(size, size)),
